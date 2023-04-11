ed7873234ea1a1cdb1bf9ed194ae0bb544615f90
RandomCocc0nut <125303373+RandomCocc0nut@users.noreply.github.com>
2023-04-11 20:42:56 +0200

    2.0 Api. To-do: Test it

--- shiny-lib.py
+++ shiny-lib.py
@@ -4,18 +4,31 @@ A library to interact with Shiny's api.
 
 import requests
 
-def Pay(customerName, customerKey, reciever, amount, appId):
- res = requests.post('https://shinies.space/pay-with-key?user='+customerName+'&key='+customerKey+'&receiver='+reciever+'&amount='+amount+'appid='+appId)
- return res.text
-
-def Request(customerName, customerKey, toRequestFrom,   amount,appId):
- res = requests.post('https://shinies.space/request-with-key?user='+customerName+'&key='+customerKey+'&torequest='+toRequestFrom+'&amount='+amount+'appid='+appId)
+def GetVersion():
+ res = requests.get('https://shinies.space/version')
  return res.text
 
 def UserInfo(username):
- res = requests.get('https://shinies.space/user-info/'+username)
+ res = requests.get('https://shinies.space/api/users/'+username)
  return res.text
 
-def ValidateKey(username, key):
- res = requests.get('https://shinies.space/validate-key?user='+username+'&key='+key)
+def UserHistory(username):
+ res = requests.get('https://shinies.space/api/history/'+username)
  return res.text
+
+def AppInfo(key):
+ res = requests.get('https://shinies.space/api/app/'+key)
+ return res.text
+
+def Pay(key, reciever, amount):
+ res = requests.post('https://shinies.space/api/pay', data={'key': key,'reciever': reciever, 'amount': amount}) 
+ return res.text
+
+def AppPay(key, app, amount):
+ res = requests.post('https://shinies.space/api/pay', data={'key': key,'app': app, 'amount': amount}) 
+ return res.text
+
+
+
+
+
