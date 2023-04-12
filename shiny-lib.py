56f7536ee2912d6a3c49133b111a197cea8dd9ed
RandomCocc0nut <125303373+RandomCocc0nut@users.noreply.github.com>
2023-04-12 17:51:54 +0200

    Some stuff

--- shiny-lib.py
+++ shiny-lib.py
@@ -28,14 +28,14 @@ def Pay(customerName, customerkey, receiver, amount):
   res = requests.post('https://shinies.space/api/pay', data={'key': customerkey,'receiver': receiver, 'amount': amount}) 
   return res.text
 
-def AppPay(customerName, key, app,receiver, amount):
+def AppPay(customerName, key, app,receiver,amount):
  if v == '2'
   res = requests.post('https://shinies.space/api/pay', data={'key': key,'app': app, 'amount': amount}) 
  elif v == '1'
   res = requests.post('https://shinies.space/pay-with-key?user='+customerName+'&key='+key+'&receiver='+receiver+'&amount='+amount+'appid='+app)
  return res.text
 
-def request(customerName, customerKey, toRequestFrom,   amount,appId):
+def request(customerName, customerKey, toRequestFrom, amount,appId):
  if v == '1':
   res = requests.post('https://shinies.space/request-with-key?user='+customerName+'&key='+customerKey+'&torequest='+toRequestFrom+'&amount='+amount+'appid='+appId)
   return res.text
