570f28b9bc1b4cc0a99f0114370d00db00ff65d5
RandomCocc0nut <125303373+RandomCocc0nut@users.noreply.github.com>
2023-04-13 07:22:12 +0200

    The code was bad

--- shiny-lib.py
+++ shiny-lib.py
@@ -6,40 +6,40 @@ import requests
 v = GetVersion() # Gets the latest version
 
 def GetVersion():
- res = requests.get('https://shinies.space/version')
- return res.text
+    res = requests.get('https://shinies.space/version')
+    return res.text
 
 def UserInfo(username):
- if v == '1':
-  res = requests.get('https://shinies.space/user-info/'+username)
- elif v == '2':
-  res = requests.get('https://shinies.space/api/users/'+username)
- return res.text
+    if v == '1':
+        res = requests.get('https://shinies.space/user-info/'+username)
+    elif v == '2':
+        res = requests.get('https://shinies.space/api/users/'+username)
+    return res.text
 
 def UserHistory(username):
- res = requests.get('https://shinies.space/api/history/'+username)
- return res.text
+    res = requests.get('https://shinies.space/api/history/'+username)
+    return res.text
 
 def AppInfo(key):
- res = requests.get('https://shinies.space/api/app/'+key)
- return res.text
+    res = requests.get('https://shinies.space/api/app/'+key)
+    return res.text
 
 def Pay(customerName, customerkey, receiver, amount):
- if v == '2'
-  res = requests.post('https://shinies.space/api/pay', data={'key': customerkey,'receiver': receiver, 'amount': amount}) 
- elif v == '1'
-  res = requests.post('https://shinies.space/pay-with-key?user='+customerName+'&key='+key+'&receiver='+receiver+'&amount='+amount+'appid='+app)
- return res.text
+    if v == '2':
+        res = requests.post('https://shinies.space/api/pay', data={'key': customerkey,'receiver': receiver, 'amount': amount}) 
+    elif v == '1':
+        res = requests.post(f'https://shinies.space/pay-with-key?user={customerName}&key={key}&receiver={receiver}&amount={amount}&appid={app}')
+    return res.text
 
 def AppPay(customerName, key, app,receiver,amount):
- if v == '2'
-  res = requests.post('https://shinies.space/api/pay', data={'key': key,'app': app, 'amount': amount}) 
- elif v == '1'
- return "App-pay is a v2 feature"
+    if v == '2':
+        res = requests.post('https://shinies.space/api/pay', data={'key': key,'app': app, 'amount': amount}) 
+    elif v == '1':
+        return "App-pay is a v2 feature"
 
 def request(customerName, customerKey, toRequestFrom, amount, appId):
- if v == '1':
-  res = requests.post('https://shinies.space/request-with-key?user='+customerName+'&key='+customerKey+'&torequest='+toRequestFrom+'&amount='+amount+'appid='+appId)
-  return res.text
- elif v == '2':
-  return "Requests are not anymore a feature in v2"
+    if v == '1':
+        res = requests.post(f'https://shinies.space/request-with-key?user={customerName}&key={customerKey}&torequest={toRequestFrom}&amount={amount}appid={appId}')
+        return res.text
+    elif v == '2':
+        return "Requests are not anymore a feature in v2"
